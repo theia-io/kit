1bdd6e8508ab7693017c68b4adc8661335664942
Bilokha Danylo <danbilokha@gmail.com>
2024-06-30 15:41:14 +0200

    feat: primeng, tailwind, layers

--- libs/ui/features/settings/ui/src/lib/experience-add/experience-add.component.ts
+++ libs/ui/features/settings/ui/src/lib/experience-add/experience-add.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject
+} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -11,15 +15,17 @@ import {
   ExperienceType,
   LocationType,
 } from '@kitouch/shared/models';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { InputTextModule } from 'primeng/inputtext';
-import { CheckboxModule } from 'primeng/checkbox';
-import { DropdownModule } from 'primeng/dropdown';
-import { CalendarModule } from 'primeng/calendar';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { ChipsModule } from 'primeng/chips';
-import { FileUploadModule } from 'primeng/fileupload';
 import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipsModule } from 'primeng/chips';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { StepperModule } from 'primeng/stepper';
 
 interface UploadEvent {
   originalEvent: Event;
@@ -41,14 +47,18 @@ interface UploadEvent {
     InputSwitchModule,
     ChipsModule,
     FileUploadModule,
+    StepperModule,
+    ButtonModule,
     //
   ],
   templateUrl: './experience-add.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [MessageService]
+  providers: [MessageService],
 })
 export class FeatSettingsExperienceAddComponent {
   #fb = inject(FormBuilder);
+
+  stepperActive = 0;
 
   experienceForm = this.#fb.group({
     title: ['', { validators: [Validators.required, Validators.minLength(2)] }],
