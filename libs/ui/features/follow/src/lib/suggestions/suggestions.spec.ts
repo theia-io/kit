b28a42d852358cf5b4db8a20605de53b8c6b5cc0
Bilokha Danylo <danbilokha@gmail.com>
2024-07-13 12:19:25 +0200

    feat: passing tests for 1st part of suggestions algorithm - intersections of worked together during same time

--- libs/ui/features/follow/src/lib/suggestions/suggestions.spec.ts
+++ libs/ui/features/follow/src/lib/suggestions/suggestions.spec.ts
@@ -1,4 +1,4 @@
-import { A, E, ExperienceDates, F, G, K, L } from './experience.mocks';
+import { A, B, C, D, E, ExperienceDates, F, G, K, L } from './experience.mocks';
 
 const getExperienceIntersection = (
   { startDate: s1, endDate: e1 }: ExperienceDates,
@@ -11,11 +11,9 @@ const getExperienceIntersection = (
     s2Number = new Date(s2).getTime(),
     e2Number = (e2 ? new Date(e2) : NOW).getTime();
 
-  // return (s1 >= s2 && s1 <= (e2 ?? (new Date).toISOString()))
-  //     || (s2 >= s1 && s2 <= (e1 ?? (new Date).toISOString()));
   return (
-    (s1Number - s2Number > 0 && e2Number - s1Number > 0) ||
-    (s2Number - s1Number > 0 && e1Number - s2Number > 0)
+    (s1Number - s2Number >= 0 && e2Number - s1Number >= 0) ||
+    (s2Number - s1Number >= 0 && e1Number - s2Number >= 0)
   );
 };
 
@@ -25,29 +23,14 @@ describe('Suggestions', () => {
       getExperienceIntersection(A, experienceDates)
     );
 
-    expect(result).toHaveLength(4);
+    expect(result).toHaveLength(5);
     expect(result[0]).toEqual(C);
     expect(result[1]).toEqual(D);
     expect(result[2]).toEqual(E);
     expect(result[3]).toEqual(F);
+    expect(result[4]).toEqual(K);
   });
 
-  const B: ExperienceDates = {
-    title: 'B',
-    startDate: '2020-03-31T22:00:00.000Z',
-    endDate: '2021-06-31T22:00:00.000Z',
-  };
-  const C: ExperienceDates = {
-    title: 'C',
-    startDate: '2020-03-31T22:00:00.000Z',
-    endDate: '2022-11-31T22:00:00.000Z',
-  };
-
-  const D: ExperienceDates = {
-    title: 'D',
-    startDate: '2020-03-31T22:00:00.000Z',
-    endDate: '2024-03-31T22:00:00.000Z',
-  };
   it('should return that B worked with C, D', () => {
     const result = [A, C, D, E, F, G, K, L].filter((experienceDates) =>
       getExperienceIntersection(B, experienceDates)
