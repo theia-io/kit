8c530b5dc86522f52dc3767672b9554dfc770b2e
Bilokha Danylo <danbilokha@gmail.com>
2024-07-31 13:26:18 +0200

    feat: farewell UI

--- libs/ui/pages/src/lib/farewell/generate/farewell-generate.component.ts
+++ libs/ui/pages/src/lib/farewell/generate/farewell-generate.component.ts
@@ -1,32 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { EditorModule, EditorTextChangeEvent } from 'primeng/editor';
 
 @Component({
   standalone: true,
   templateUrl: './farewell-generate.component.html',
   imports: [
-    ReactiveFormsModule,
+    // ReactiveFormsModule,
     //
+    EditorModule,
     FloatLabelModule,
     InputTextModule,
     ButtonModule,
   ],
 })
 export class FarewellGenerateComponent {
-  farewellControl = new FormControl<string>('', [
-    Validators.required,
-    Validators.minLength(100),
-    Validators.maxLength(10000),
-  ]);
+  modules = {
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link'],
+      
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      
+        ['clean']                                         // remove formatting button
+      ]
+    };
+
+  editorText = signal<EditorTextChangeEvent | null>(null);
+  textValue = computed(() => this.editorText()?.textValue ?? '');
+
+  // farewellControl = new FormControl<string>('', [
+  //   Validators.required,
+  //   Validators.minLength(100),
+  //   Validators.maxLength(10000),
+  // ]);
+
+  onTextChangeHandler(value: EditorTextChangeEvent) {
+    this.editorText.set(value);
+  }
 
   saveFarewellHandler() {
-    if (!this.farewellControl.valid) {
-      return;
-    }
-
-    console.log(this.farewellControl.value);
+    console.log(this.editorText());
   }
 }
