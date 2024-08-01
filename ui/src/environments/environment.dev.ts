f7be190cae49d8fcb2daf029665113449bd96706
Bilokha Danylo <danbilokha@gmail.com>
2024-08-01 22:23:21 +0200

    feat: dev/prd separation

--- ui/src/environments/environment.dev.ts
+++ ui/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 import { Environment } from '@kitouch/ui-shared';
 
 export const environment: Environment = {
-  realmAppId: 'application-0-gnmmqxd',
-  development: false,
+  realmAppId: 'application-0-gwcbmrg',
+  development: true,
   production: true,
 };
