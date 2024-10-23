9c2776bdaacea651f25338cecb83628216407bda
Bilokha Danylo <danbilokha@gmail.com>
2024-10-23 09:00:56 +0200

    chore: lint fix, self review

--- libs/shared/emoji/src/lib/emoji.ts
+++ libs/shared/emoji/src/lib/emoji.ts
@@ -11,13 +11,3 @@ export const emojiNameMap = new Map<string, string>();
 Object.entries(emoji).forEach(([emojiSymbol, names]) =>
   names.forEach((name) => emojiNameMap.set(name, emojiSymbol))
 );
-
-// export const emojiData = Object.entries(emojilib).map(
-//   ([name, { char: emoji }]) => [name, emoji] as const,
-// )
-
-// export const emojiCodesByName = new Map(emojiData)
-
-// export const emojiNamesByCode = new Map(
-//   emojiData.map(([name, emoji]) => [normalizeCode(emoji), name]),
-// )
