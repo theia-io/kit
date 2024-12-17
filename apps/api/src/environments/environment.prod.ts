cb37d80a402d4e5d213decc23b4a46719b6b6132
Bilokha Danylo <danbilokha@gmail.com>
2024-12-17 16:50:53 +0100

    fix: updated string interpalation

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from './types';
 
 export const environment: Environment = {
   production: true,
-  version: '${version}',
-  apiBase: '${apiBase}',
+  version: '${{ version }}',
+  apiBase: '${{ apiBase }}',
 };
