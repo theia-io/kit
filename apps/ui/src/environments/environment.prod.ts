8fe5bfc51002c830c99799df81f969a6fd4a15fe
Bilokha Danylo <danbilokha@gmail.com>
2024-12-19 19:34:31 +0100

    feat: infra

--- apps/ui/src/environments/environment.prod.ts
+++ apps/ui/src/environments/environment.prod.ts
@@ -1,32 +1,12 @@
 import { Environment, KIT_ENVS } from '@kitouch/shared-infra';
 
-// const version = '${PACKAGE_VERSION}';
-// const version2 = '${{ PACKAGE_VERSION }}';
-// const uiVersion = '${UI_VERSION}';
-// const uiVersion2 = '${ UI_VERSION }';
-// const cmdVersion = '${version}';
-// const cmdVersion2 = '${{version}}';
-
-// console.log(
-//   `\n\nversion: ${version}, uiVersion: ${uiVersion}, cmdVersion: ${cmdVersion}`
-// );
-
 export const environment: Environment = {
-  // test: {
-  //   version,
-  //   version2,
-  //   uiVersion,
-  //   uiVersion2,
-  //   cmdVersion,
-  //   cmdVersion2,
-  //   testVariable: '${{ API_BASE }} ',
-  // },
   api: {
-    root: 'http://kit-api-alb-819910982.eu-north-1.elb.amazonaws.com/api',
+    root: '$API_BASE',
     media: '/media',
   },
   googleTagConfig: 'G-7YL998B39Q',
-  build: KIT_ENVS.production + `-5.0.42-56e86ca}`,
+  build: KIT_ENVS.production + '-5-' + '$AWS_COMMIT_ID',
   realmAppId: 'application-0-gnmmqxd',
   environment: KIT_ENVS.production,
   production: true,
