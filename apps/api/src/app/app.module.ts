1a6e06dcf4ec88ee86f3ecfc8d00b0eb7aa4f9ac
Dan <danbilokha@gmail.com>
2024-12-20 11:55:01 +0100

    feat: added basic terminus healthchecks api (#58)

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { MediaModule } from '@kitouch/be-media';
 import { InfraModule } from '@kitouch/infra';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config';
+import { HealthController } from './heath.controller';
 
 @Module({
-  imports: [ConfigModule, InfraModule, MediaModule],
-  controllers: [AppController],
+  imports: [TerminusModule, ConfigModule, InfraModule, MediaModule],
+  controllers: [HealthController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
