ac163ef24d9acedd17ac452caa4e01deea2e16a9
Bilokha Danylo <danbilokha@gmail.com>
2025-04-18 10:59:49 +0200

    feat: generated structure

---
+++ libs/api/features/kudoboard/src/lib/schemas/tweet.schema.ts
@@ -0,0 +1,81 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument, Types } from 'mongoose'; // Import mongoose for Types.ObjectId if needed
+
+export type TweetDocument = HydratedDocument<Tweet>;
+
+@Schema({
+  _id: false, // <<< Set _id to false if you DON'T need a separate MongoDB ObjectId for each comment within the array
+  timestamps: true,
+})
+export class TweetComment {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId, // Store as ObjectId
+    ref: 'Profile', // Reference the 'Profile' model
+    required: true,
+    index: true, // Good to index if you query by commenter
+  })
+  profileId: Types.ObjectId; // Use Mongoose Types.ObjectId for refs
+
+  @Prop({ required: true }) // Add trim for content
+  content: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+export const TweetCommentSchema = SchemaFactory.createForClass(TweetComment);
+
+@Schema({
+  timestamps: true,
+  collection: 'tweet',
+  toJSON: {
+    virtuals: true, // <<< ENSURE this is true (or omit, as true is default)
+    versionKey: false, // Optional: Remove the __v field
+    transform(doc, ret) {
+      delete ret._id; // <<< Remove the original _id field from the output
+      // You can add other transformations here if needed
+    },
+  },
+})
+export class Tweet {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Profile',
+    required: true,
+  })
+  profileId: Types.ObjectId;
+
+  // TODO @FIXME DB has denormalization of the tweet so to avoid left join on Tweet API which is supposed to be the most bombarded :/ Potentially store "related profiles"
+  // @Prop()
+  // denormalization:
+
+  @Prop({
+    required: true,
+  })
+  content: string;
+
+  @Prop({ type: [TweetCommentSchema], default: [] }) // Use the TweetCommentSchema in an array type
+  comments: TweetComment[];
+
+  @Prop()
+  type: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }],
+    default: [],
+    ref: 'Profile',
+  })
+  upProfileIds: Types.ObjectId[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }],
+    default: [],
+    ref: 'Profile',
+  })
+  downProfileIds: Types.ObjectId[];
+}
+
+export const TweetSchema = SchemaFactory.createForClass(Tweet);
